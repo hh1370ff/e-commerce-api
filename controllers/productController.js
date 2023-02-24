09b11f4fb4ffc62e919f266d6739f4eea074f296
Hussein <Husseinfarsangi1370@gmail.com>
2023-02-24 10:26:39 +0330

    create product routes

--- controllers/productController.js
+++ controllers/productController.js
@@ -57,10 +57,7 @@ export const updateProduct = asyncHandler(async (req, res) => {
     throw new Error("Product not found", 400);
   }
 
-  const updated = await product.update(
-    { ...req.body }
-   
-  );
+  const updated = await product.update({ ...req.body });
 
   res.json({ message: `Item is updated` });
 });
@@ -78,4 +75,49 @@ export const getAllProduct = asyncHandler(async (req, res) => {
   }
 
   res.json(products);
+});
+
+// @desc Delete a product
+// @route DELETE /deleteProduct
+// @access Private
+export const deleteProduct = asyncHandler(async (req, res) => {
+  const { _id } = req.body;
+
+  // Confirm data
+  if (!_id) {
+    throw new Error("Product ID Required", 400);
+  }
+
+  // Does the product exist to delete?
+  const product = await Product.findById(_id).exec();
+
+  if (!product) {
+    throw new Error("Product not found", 400);
+  }
+
+  const result = await product.deleteOne();
+
+  const reply = `Product${result.title} with ID ${result._id} deleted`;
+
+  res.json(reply);
+});
+
+// @desc Get a singleProduct
+// @route GET /singleProduct
+// @access Private
+export const getSingleProduct = asyncHandler(async (req, res) => {
+  const { _id } = req.params;
+
+  // Confirm data
+  if (!_id) throw new Error("ID is required", 400);
+
+  // Get single product from MongoDB
+  const product = await Product.findById(_id).lean();
+
+  // If no product
+  if (!product) {
+    throw new Error("No products found", 400);
+  }
+
+  res.json(product);
 });
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,7 +1,9 @@
 import express from "express";
 import {
   createProduct,
+  deleteProduct,
   getAllProduct,
+  getSingleProduct,
   updateProduct,
 } from "../controllers/productController.js";
 const productRouter = express.Router();
@@ -9,5 +11,7 @@ const productRouter = express.Router();
 productRouter.route("/createProduct").post(createProduct);
 productRouter.route("/updateProduct").put(updateProduct);
 productRouter.route("/getAllProducts").get(getAllProduct);
+productRouter.route("/deleteProduct").delete(deleteProduct);
+productRouter.route("/getSingleProduct/:_id").get(getSingleProduct);
 
 export default productRouter;
